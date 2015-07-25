25da69bf94d1b5b3c5d921aed884fabb0d57fbdc
Shakil Kanji <shakil.kanji@gmail.com>
2015-07-24 20:06:09 -0400

    Initial Commit

---
+++ VoiceFX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VoiceFX
+//
+//  Created by Shakil Kanji on 2015-07-24.
+//  Copyright (c) 2015 Shakil Kanji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
